50b961f8aa44680e1645262b44b6b727200df790
miho_kobayashi <m.koba17@gmail.com>
2022-04-07 13:31:14 +0000

    2-2 仮のトップページを表示迄

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+   # ここにtopコントローラーのindexアクションのルーティングを追加する
+  get 'top/index'
+  
 end
